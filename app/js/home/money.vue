7f08a42b9572c78b485d1ef4c0d10e08b6f08a7f
tom20170215 <tom_tyceshi@163.com>
2018-04-28 20:31:34 +0800

    0428

---
+++ app/js/home/money.vue
@@ -0,0 +1,25 @@
+<template>
+	<Panel title="理财精选" :class="$style.panel">
+		<section :class="$style.content">
+			
+		</section>
+	</Panel>
+</template>
+
+<script>
+import Panel from "../core/panel.vue"
+export default {
+
+  name: 'money',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+	@import "../../css/element.scss" 
+</style>

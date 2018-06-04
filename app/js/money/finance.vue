18bf5180cfdb4e7bfdf90778434834a3245e7076
tom20170215 <tom_tyceshi@163.com>
2018-06-04 21:45:19 +0800

    app/js/money router.js navbar.vue

---
+++ app/js/money/finance.vue
@@ -0,0 +1,46 @@
+<template>
+	<Panel title="理财精选" :class="$style.panel">
+		<section :class="$style.content">
+			<div :class="$style.item">
+				<img src="//img12.360buyimg.com/jrpmobile/jfs/t3670/58/543125556/2606/a65096b9/580d80afN63b1085a.png?width=76&height=76" alt="">
+				<div>
+					<h4>月度理财</h4>
+					<p>预期年化收益率4.50%</p>
+				</div>
+				<em>去赚钱 ></em>
+			</div>
+		</section>
+	</Panel>
+</template>
+
+<script>
+import Panel from '../core/panel.vue';
+export default {
+	components:{
+		Panel,
+	},
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+	@import "../../css/element.scss";
+	.panel{
+		@include panel;
+		.content{
+			@include flex;
+			.item{
+			@include flex(row);
+			>img{
+				width:80px;
+				height:88px;
+			}
+			}
+		} 
+	}
+</style>
---
+++ app/js/money/index.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+    <div>
+        <Heador/>
+        <Slider/>	
+        <Ada/>
+        <Money/>
+        <Adb/>
+        <Finance/>
+        <Footer/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue';
+import Footer from '../public/footer.vue';
+import Navbar from '../public/navbar.vue';
+import Slider from './mslider.vue';
+import Ada from './ada.vue';
+import Money from './money.vue';
+import Adb from './adb.vue';
+import Finance from './finance.vue';
+export default {
+	components:{
+		Heador,
+		Slider,
+		Ada,
+		Money,
+		Adb,
+		Finance,
+		Footer,
+		Navbar,
+
+	}
+}
+</script>
+
+<style lang="scss" module>
+</style>
